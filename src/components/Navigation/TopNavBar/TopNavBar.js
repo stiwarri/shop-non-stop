99918964352aab080dc8f89712091591bd517f8a
Shubham Tiwari <shubham.tiwari1097@gmail.com>
2020-04-12 18:56:40 +0530

    Styled TopNavBar component

--- src/components/Navigation/TopNavBar/TopNavBar.js
+++ src/components/Navigation/TopNavBar/TopNavBar.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./TopnavBar.scss";
 
 const TopNavBar = () => {
-    return (
-        <ul>
-            <li>
-                <NavLink to='/'>Home</NavLink>
-            </li>
-            <li>
-                <NavLink to='/shop'>Shop</NavLink>
-            </li>
-            <li>
-                <NavLink to='/about-us'>About Us</NavLink>
-            </li>
-        </ul>
-    )
-}
+  return (
+    <ul>
+      <li>
+        <span className="logo">ShopNonStop</span>
+      </li>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/shop">Shop</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about-us">About Us</NavLink>
+      </li>
+    </ul>
+  );
+};
 
 export default TopNavBar;
