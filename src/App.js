f41654c00966e4f55116898968405d3c9ff674dd
Shubham Tiwari <shubham.tiwari1097@gmail.com>
2020-06-24 22:40:47 +0530

    Performance Optimizations: Add code splitting (Lazy Loading) for HomePage, ShopPage and CheckoutPage components.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.scss';
-import CheckoutPage from './pages/CheckoutPage/CheckoutPage.jsx';
-import HomePage from './pages/HomePage/HomePage.jsx';
 import Layout from './hoc/Layout/Layout.jsx';
 import Modal from './components/UI/Modal/Modal.jsx';
-import ShopPage from './pages/ShopPage/ShopPage.jsx';
+import Spinner from './components/UI/Spinner/Spinner';
 import TopNavBar from './components/Navigation/TopNavBar/TopNavBar.jsx';
-import SignInSignUpPage from './pages/SignInSignUpPage/SignInSignUpPage.jsx';
 
 import * as modalActionCreators from './redux/actions/modalAction';
 import * as authActionCreators from './redux/actions/authAction';
 import { showModalSelector, modalMessageSelector } from './redux/selectors/modalSelector';
 import { authStatusSelector } from './redux/selectors/authSelector';
 
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const ShopPage = lazy(() => import('./pages/ShopPage/ShopPage.jsx'));
+const SignInSignUpPage = lazy(() => import('./pages/SignInSignUpPage/SignInSignUpPage.jsx'));
+const CheckoutPage = lazy(() => import('./pages/CheckoutPage/CheckoutPage.jsx'));
+
 class App extends React.Component {
     render() {
         let routes = (
             <Switch>
-                <Route path='/' exact component={HomePage} />
-                <Route path='/shop' component={ShopPage} />
-                <Route path='/sign-in' component={SignInSignUpPage} />
-                <Redirect to='/' />
+                <Suspense fallback={<Spinner />}>
+                    <Route path='/' exact component={HomePage} />
+                    <Route path='/shop' component={ShopPage} />
+                    <Route path='/sign-in' component={SignInSignUpPage} />
+                    <Redirect to='/' />
+                </Suspense>
             </Switch>
         );
 
         if (this.props.authStatus) {
             routes = (
                 <Switch>
-                    <Route path='/' exact component={HomePage} />
-                    <Route path='/shop' component={ShopPage} />
-                    <Route path='/checkout' component={CheckoutPage} />
-                    <Redirect to='/' />
+                    <Suspense fallback={<Spinner />}>
+                        <Route path='/' exact component={HomePage} />
+                        <Route path='/shop' component={ShopPage} />
+                        <Route path='/checkout' component={CheckoutPage} />
+                        <Redirect to='/' />
+                    </Suspense>
                 </Switch>
             );
         }
--- src/containers/SignIn/SignIn.jsx
+++ src/containers/SignIn/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
@@ -6,196 +6,198 @@ import './SignIn.scss';
 import SIGN_IN_FORM_CONFIG from '../../assets/config/sign-in-form';
 import FormInput from '../../components/UI/FormInput/FormInput';
 import Button from '../../components/UI/Button/Button.jsx';
-import { updateObject } from '../../utils/appHelper';
-
 import * as authActionCreators from '../../redux/actions/authAction';
 
-const initialState = {
-    signInForm: SIGN_IN_FORM_CONFIG,
-    isFormValid: false
-};
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'updateValidity':
-            return updateObject(state, {
-                signInForm: updateObject(state.signInForm, {
-                    [action.elementKey]: updateObject(state.signInForm[action.elementKey], {
-                        status: updateObject(state.signInForm[action.elementKey].status, {
-                            isValid: action.validity
-                        })
-                    })
-                })
-            });
-
-        case 'updateTouchedStatus':
-            return updateObject(state, {
-                signInForm: updateObject(state.signInForm, {
-                    [action.elementKey]: updateObject(state.signInForm[action.elementKey], {
-                        status: updateObject(state.signInForm[action.elementKey].status, {
-                            isTouched: action.touched
-                        })
-                    })
-                })
-            });
-
-        case 'updateErrorMessage':
-            return updateObject(state, {
-                signInForm: updateObject(state.signInForm, {
-                    [action.elementKey]: updateObject(state.signInForm[action.elementKey], {
-                        errorMessage: action.errMsg
-                    })
-                })
-            });
-
-        case 'updateValue':
-            return updateObject(state, {
-                signInForm: updateObject(state.signInForm, {
-                    [action.elementKey]: updateObject(state.signInForm[action.elementKey], {
-                        properties: updateObject(state.signInForm[action.elementKey].properties, {
-                            value: action.value
-                        })
-                    })
-                })
-            });
-
-        case 'updateFormValidity':
-            return updateObject(state, {
-                isFormValid: action.formValidity
-            });
-
-        default:
-            return state;
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            signInForm: SIGN_IN_FORM_CONFIG,
+            isFormValid: false
+        };
     }
-};
 
-const SignIn = ({ history, googleSignIn, signIn }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    let formElements = [];
-    for (let el in state.signInForm) {
-        formElements.push({
-            key: el,
-            ...state.signInForm[el]
+    inputFocusHandler = (event, elementKey) => {
+        this.setState({
+            signInForm: {
+                ...this.state.signInForm,
+                [elementKey]: {
+                    ...this.state.signInForm[elementKey],
+                    status: {
+                        ...this.state.signInForm[elementKey].status,
+                        isValid: true,
+                        isTouched: true
+                    }
+                }
+            }
         });
-    }
-
-    const inputFocusHandler = (event, elementKey) => {
-        dispatch({ type: 'updateValidity', elementKey: elementKey, validity: true });
-        dispatch({ type: 'updateTouchedStatus', elementKey: elementKey, touched: true });
     };
 
-    const inputBlurHandler = (event, elementKey) => {
-        const [validity, errMsg] = checkElementValidity(state.signInForm[elementKey].validations, event.target.value);
-        dispatch({ type: 'updateValidity', elementKey: elementKey, validity: validity });
-        dispatch({ type: 'updateErrorMessage', elementKey: elementKey, errMsg: errMsg });
+    inputBlurHandler = (event, elementKey) => {
+        const [validity, errMsg] = this.checkElementValidity(this.state.signInForm[elementKey].validations, event.target.value);
+
+        this.setState({
+            signInForm: {
+                ...this.state.signInForm,
+                [elementKey]: {
+                    ...this.state.signInForm[elementKey],
+                    status: {
+                        ...this.state.signInForm[elementKey].status,
+                        isValid: validity
+                    },
+                    errorMessage: errMsg
+                }
+            }
+        });
     };
 
-    const inputChangeHandler = (event, elementKey) => {
+    inputChangeHandler = (event, elementKey) => {
         const inputValue = event.target.value.trimStart();
-        const [validity, errMsg] = checkElementValidity(state.signInForm[elementKey].validations, inputValue);
-        dispatch({ type: 'updateValidity', elementKey: elementKey, validity: validity });
-        dispatch({ type: 'updateErrorMessage', elementKey: elementKey, errMsg: errMsg });
-        dispatch({ type: 'updateValue', elementKey: elementKey, value: inputValue });
-        const formValidity = checkFormValidity();
-        dispatch({ type: 'updateFormValidity', elementKey: elementKey, formValidity: formValidity });
+        const [validity, errMsg] = this.checkElementValidity(this.state.signInForm[elementKey].validations, inputValue);
+
+        this.setState({
+            signInForm: {
+                ...this.state.signInForm,
+                [elementKey]: {
+                    ...this.state.signInForm[elementKey],
+                    properties: {
+                        ...this.state.signInForm[elementKey].properties,
+                        value: inputValue
+                    },
+                    status: {
+                        ...this.state.signInForm[elementKey].status,
+                        isValid: validity
+                    },
+                    errorMessage: errMsg
+                }
+            }
+        }, () => {
+            const formValidity = this.checkFormValidity();
+            this.setState({
+                isFormValid: formValidity
+            });
+        });
     };
 
-    const checkElementValidity = (validations, inputValue) => {
-        let isValid = true,
-            errMsg = '';
-
+    checkElementValidity = (validations, inputValue) => {
+        let isValid = true;
+        let errMsg = '';
         for (let rule in validations) {
             switch (rule) {
                 case 'required':
                     isValid = inputValue ? true : false;
                     if (!isValid) errMsg = 'Required';
                     break;
-
                 case 'minLength':
                     isValid = inputValue.length >= validations[rule];
                     if (!isValid) errMsg = `Minimum length ${validations[rule]} is required`;
                     break;
-
                 case 'maxLength':
                     isValid = inputValue.length <= validations['maxLength'];
                     if (!isValid) errMsg = `Maximum character limit: ${validations['maxLength']}`;
                     break;
-
                 case 'regex':
                     isValid = validations[rule].test(inputValue);
                     if (!isValid) errMsg = `Invalid`;
                     break;
-
                 default:
                     isValid = 'true';
                     errMsg = '';
             }
-
             if (!isValid) return [isValid, errMsg];
         }
-
         return [isValid, errMsg];
     };
 
-    const checkFormValidity = () => {
+    checkFormValidity = () => {
         let isValid = true;
-
-        for (let element in state.signInForm) {
-            if (!state.signInForm[element].status.isValid) {
+        for (let element in this.state.signInForm) {
+            if (!this.state.signInForm[element].status.isValid) {
                 isValid = false;
                 return isValid;
             }
         }
-
         return isValid;
     };
 
-    const formSubmitHandler = async event => {
+    formSubmitHandler = async event => {
         let inputValues = {};
-        for (let el in state.signInForm) {
-            inputValues[el] = state.signInForm[el].properties.value;
+        for (let el in this.state.signInForm) {
+            inputValues[el] = this.state.signInForm[el].properties.value;
         }
-        signIn(inputValues.email, inputValues.password, history);
-        resetFormHandler();
+        this.props.signIn(inputValues.email, inputValues.password, this.props.history);
+        this.resetFormHandler();
     };
 
-    const resetFormHandler = () => {
-        for (let element in state.signInForm) {
-            dispatch({ type: 'updateValue', elementKey: element, value: '' })
-            dispatch({ type: 'updateValidity', elementKey: element, validity: false });
-            dispatch({ type: 'updateTouchedStatus', elementKey: element, touched: false });
-        }
-        dispatch({ type: 'updateFormValidity', formValidity: false });
-    };
-
-    return (
-        <div className="sign-in">
-            <h2>I already have an account</h2>
-            <h4>Sign in with your email and password.</h4>
-            <form>
-                {
-                    formElements.map(el => (
-                        <FormInput
-                            key={el.key}
-                            label={el.label}
-                            properties={el.properties}
-                            status={el.status}
-                            errorMessage={el.errorMessage}
-                            handleFocus={event => inputFocusHandler(event, el.key)}
-                            handleBlur={event => inputBlurHandler(event, el.key)}
-                            handleChange={event => inputChangeHandler(event, el.key)}
-                        />
-                    ))
+    resetFormHandler = () => {
+        this.setState({
+            signInForm: {
+                ...this.state.signInForm,
+                email: {
+                    ...this.state.signInForm.email,
+                    properties: {
+                        ...this.state.signInForm.email.properties,
+                        value: ''
+                    },
+                    status: {
+                        ...this.state.signInForm.email.status,
+                        isValid: false,
+                        isTouched: false
+                    }
+                },
+                password: {
+                    ...this.state.signInForm.password,
+                    properties: {
+                        ...this.state.signInForm.password.properties,
+                        value: ''
+                    },
+                    status: {
+                        ...this.state.signInForm.password.status,
+                        isValid: false,
+                        isTouched: false
+                    }
                 }
-                <div className="buttons">
-                    <Button disable={!state.isFormValid} clickHandler={formSubmitHandler}>Sign In</Button>
-                    {<Button clickHandler={resetFormHandler}>Reset</Button>}
-                    <Button clickHandler={() => googleSignIn(history)} isThirdPartySignInButton>Sign In With Google</Button>
-                </div>
-            </form>
-        </div>
-    );
+            },
+            isFormValid: false
+        });
+    };
 
+    render() {
+        let formElements = [];
+        for (let el in this.state.signInForm) {
+            formElements.push({
+                key: el,
+                ...this.state.signInForm[el]
+            });
+        }
+        return (
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <h4>Sign in with your email and password.</h4>
+                <form>
+                    {
+                        formElements.map(el => (
+                            <FormInput
+                                key={el.key}
+                                label={el.label}
+                                properties={el.properties}
+                                status={el.status}
+                                errorMessage={el.errorMessage}
+                                handleFocus={event => this.inputFocusHandler(event, el.key)}
+                                handleBlur={event => this.inputBlurHandler(event, el.key)}
+                                handleChange={event => this.inputChangeHandler(event, el.key)}
+                            />
+                        ))
+                    }
+                    <div className="buttons">
+                        <Button disable={!this.state.isFormValid} clickHandler={this.formSubmitHandler}>Sign In</Button>
+                        <Button clickHandler={this.resetFormHandler}>Reset</Button>
+                        <Button clickHandler={() => this.props.googleSignIn(this.props.history)} isThirdPartySignInButton>Sign In With Google</Button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 const mapDispatchToProps = dispatch => {
