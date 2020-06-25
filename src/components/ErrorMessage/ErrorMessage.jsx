e2f7d12c0e2738375089bab8f416504cb0ee2fbb
Shubham Tiwari <shubham.tiwari1097@gmail.com>
2020-06-25 16:38:01 +0530

    App Improvement: Add error message logo.

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './ErrorMessage.scss';
+import ErrorImage from '../../assets/images/error.png';
 
 const ErrorMessage = props => {
     return (
         <div className="error-page">
-            <img src="https://i.imgur.com/lKJiT77.png" alt="error" />
+            <img src={ErrorImage} alt="error" />
             <h2>Oops! Something went wrong.</h2>
         </div>
     );
