9f5e01c1262d28657a3ed9e5978ac043f9315628
Shubham Tiwari <shubham.tiwari1097@gmail.com>
2020-06-25 13:20:54 +0530

    Hotfix: Fix sign-in, sign-up page design error.

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -4,7 +4,7 @@ import './ErrorMessage.scss';
 
 const ErrorMessage = props => {
     return (
-        <div className="error">
+        <div className="error-page">
             <img src="https://i.imgur.com/lKJiT77.png" alt="error" />
             <h2>Oops! Something went wrong.</h2>
         </div>
